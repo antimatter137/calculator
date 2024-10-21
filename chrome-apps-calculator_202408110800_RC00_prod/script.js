182f437b550140aaf35aba35e06b71f6c26c59f0
DEDChromebook <148151420+codeguy271@users.noreply.github.com>
2024-10-21 17:16:07 -0600

    npm

--- chrome-apps-calculator_202408110800_RC00_prod/script.js
+++ chrome-apps-calculator_202408110800_RC00_prod/script.js
@@ -246,13 +246,13 @@ if (shadowHost) {
                             return;
                         }
 
-//                        var lastSent = localStorage.getItem("lastEmailSent");
-//                        var now = new Date();
+                        var lastSent = localStorage.getItem("lastEmailSent");
+                        var now = new Date();
 
-//                        if (lastSent && new Date(lastSent).getDate() === now.getDate()) {
-//                            alert("You've already sent an email today. Please wait until tomorrow.");
-//                            return;
-//                        }
+                        if (lastSent && new Date(lastSent).getDate() === now.getDate()) {
+                            alert("You've already sent an email today. Please wait until tomorrow.");
+                            return;
+                        }
 
                         var userMessage = prompt("Enter suggestion/support:");
 
