3f54f57e8521b011f18f250c8dbce2cf3175adf5
DEDChromebook <148151420+codeguy271@users.noreply.github.com>
2024-10-21 09:10:41 -0600

    Update script.js

--- chrome-apps-calculator_202408110800_RC00_prod/script.js
+++ chrome-apps-calculator_202408110800_RC00_prod/script.js
@@ -246,13 +246,13 @@ script.onload = function() {
     if (!confirm("If you want to help improve this website, press OK to send a suggestion, or just press Cancel to continue using the calculator.")) {
         return;
     }
-        var lastSent = localStorage.getItem("lastEmailSent");
-        var now = new Date();
+//        var lastSent = localStorage.getItem("lastEmailSent");
+//        var now = new Date();
 
-        if (lastSent && new Date(lastSent).getDate() === now.getDate()) {
-            alert("You've already sent an email today. Please wait until tomorrow.");
-            return;
-        }
+//        if (lastSent && new Date(lastSent).getDate() === now.getDate()) {
+//            alert("You've already sent an email today. Please wait until tomorrow.");
+//            return;
+//        }
 
         var userMessage = prompt("Enter suggestion/support:");
 
